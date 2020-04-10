04a1ddc53e6c41b3bc13a351aa5c228cc83fb512
Robin Leroy <egg.robin.leroy@gmail.com>
2020-04-10 13:53:09 +0200

    principia_make

---
+++ principia_make.sh
@@ -0,0 +1,12 @@
+PRINCIPIA_MACOS_C_FLAGS=
+
+principia_configure
+
+cmake \
+    -DCMAKE_C_COMPILER:FILEPATH=`which clang` \
+    -DCMAKE_CXX_COMPILER:FILEPATH=`which clang++` \
+    -DCMAKE_C_FLAGS="${C_FLAGS}" \
+    -DCMAKE_CXX_FLAGS="${CXX_FLAGS}" \
+    -DCMAKE_LD_FLAGS="${LD_FLAGS}" \
+    -DBENCHMARK_ENABLE_GTEST_TESTS=OFF
+make -j8
